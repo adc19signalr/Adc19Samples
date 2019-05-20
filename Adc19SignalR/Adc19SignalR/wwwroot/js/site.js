c63aca6088ab77da7a3c5ff2d0f718732096c9e7
viekon05 <Konstantin.Kletzander@andritz.com>
2019-05-20 12:28:10 +0200

    implement JS client

--- Adc19SignalR/Adc19SignalR/wwwroot/js/site.js
+++ Adc19SignalR/Adc19SignalR/wwwroot/js/site.js
@@ -1,4 +1,55 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/adcHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
 
-// Write your Javascript code.
+connection.on("EmpfangeNachricht", (msg) => {
+    $('#signalr-message-panel').prepend($('<div />').text(msg));
+});
+
+$('#btn-broadcast').click(function () {
+    var msg = $('#broadcast').val();
+    connection.invoke("BroadcastMessage", msg).catch(err => console.error(err.toString()));
+});
+
+$('#btn-self-message').click(function () {
+    var msg = $('#self-message').val();
+    connection.invoke("SendToCaller", msg).catch(err => console.error(err.toString()));
+});
+
+$('#btn-others-message').click(function () {
+    var msg = $('#others-message').val();
+    connection.invoke("SendToOthers", msg).catch(err => console.error(err.toString()));
+});
+
+$('#btn-group-message').click(function () {
+    var msg = $('#group-message').val();
+    var group = $('#group-for-message').val();
+    connection.invoke("SendToGroup", group, msg).catch(err => console.error(err.toString()));
+});
+
+$('#btn-group-add').click(function () {
+    var group = $('#group-to-add').val();
+    connection.invoke("AddUserToGroup", group).catch(err => console.error(err.toString()));
+});
+
+$('#btn-group-remove').click(function () {
+    var group = $('#group-to-remove').val();
+    connection.invoke("RemoveUserFromGroup", group).catch(err => console.error(err.toString()));
+});
+
+async function start() {
+    try {
+        await connection.start();
+        console.log('---connected---');
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 6000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+start();
